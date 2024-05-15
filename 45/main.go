87cc57248f25cf87c18bd3510a26a1130d20c9ed
yangshimiao <1843121593@qq.copm>
2024-05-15 23:49:01 +0800

    [add] 45.跳跃游戏 II

---
+++ 45/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// 方法1：走到最大边界后才跳
+func jump(nums []int) int {
+	rightMost, end, steps := 0, 0, 0
+	for i := 0; i < len(nums)-1; i++ {
+		rightMost = max(rightMost, i+nums[i])
+		if i == end { // 走到最大边界后才跳
+			end = rightMost
+			steps++
+		}
+	}
+	return steps
+}
+
+func main() {
+	fmt.Println(jump([]int{1, 1, 1, 1}))
+}
