cdf3b31e46fcac2332ad8f9a7f5939761ea1fde7
yangshimiao <1843121593@qq.copm>
2024-07-08 23:15:26 +0800

    13. 罗马数字转整数

--- 13/main.go
+++ 13/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 var data = map[string]int{
 	"IV": 4,
@@ -34,6 +36,27 @@ func romanToInt(s string) int {
 	return result
 }
 
+var symbolValues = map[byte]int{'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+// 方法2：先看两个，如果左边小于右边，则减少；左边大于等于右边，则增加
+func romanToInt2(s string) (ans int) {
+	n := len(s)
+	for i := range s {
+		value := symbolValues[s[i]]
+		if i < n-1 && value < symbolValues[s[i+1]] {
+			ans -= value
+		} else {
+			ans += value
+		}
+	}
+	return
+}
+
+/*
+方法1和方法2只有在输入是正确罗马数字的情况下输出才是正确的结果
+*/
+
 func main() {
-	fmt.Println(romanToInt("MCMXCIV"))
+	fmt.Println(romanToInt("CL"))
+	fmt.Println(romanToInt2("CL"))
 }
