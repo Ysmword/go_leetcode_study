61bba1f1c8672fc069e8db53e16914e666dae270
yangshimiao <1843121593@qq.copm>
2024-10-23 22:58:07 +0800

    173. 二叉搜索树迭代器

---
+++ 173/main.go
@@ -0,0 +1,39 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+type BSTIterator struct {
+	arr []int
+}
+
+// 将中序遍历的结果求出来，然后根据next和hasNext来实现迭代器
+func Constructor(root *TreeNode) BSTIterator {
+	b := BSTIterator{
+		arr: make([]int, 0),
+	}
+	inorder(root, b.arr)
+	return b
+}
+
+func inorder(root *TreeNode, arr []int) {
+	if root == nil {
+		return
+	}
+	inorder(root.Left, arr)
+	arr = append(arr, root.Val)
+	inorder(root.Right, arr)
+}
+
+func (b *BSTIterator) Next() int {
+	val := b.arr[0]
+	b.arr = b.arr[1:]
+	return val
+}
+
+func (b *BSTIterator) HasNext() bool {
+	return len(b.arr) != 0
+}
